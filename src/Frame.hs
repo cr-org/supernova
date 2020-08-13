6dae37b9f72140874d905771d727ff9d83f47178
Gabriel Volpe <volpegabriel@gmail.com>
2020-08-13 23:33:48 +0200

    distinguishing between simple and payload commands

--- src/Frame.hs
+++ src/Frame.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Frame
   ( encodeBaseCommand
   )
@@ -9,9 +11,12 @@ import qualified Data.ByteString.Lazy          as BL
 import qualified Data.ByteString.Char8         as C
 import qualified Data.ByteString.Lazy.Char8    as CL
 import           Data.Int                       ( Int32 )
+import           Data.Maybe                     ( isNothing )
 import qualified Data.ProtoLens.Encoding       as PL
 import qualified Data.ProtoLens.Encoding.Bytes as PL
+import           Lens.Family
 import           Proto.PulsarApi                ( BaseCommand )
+import qualified Proto.PulsarApi_Fields        as F
 
 {- MaxFrameSize is defined by the Pulsar spec with a single
  - sentence: "The maximum allowable size of a single frame is 5 MB."
@@ -20,19 +25,18 @@ import           Proto.PulsarApi                ( BaseCommand )
  -}
 --maxFrameSize = 5 * 1024 * 1024 -- 5mb
 
-data Frame = SimpleFrame SimpleCommand | PayloadFrame PayloadCommand
+data Frame = SimpleFrame Simple | PayloadFrame Simple Payload
 
 -- Simple command: http://pulsar.apache.org/docs/en/develop-binary-protocol/#simple-commands
-data SimpleCommand = SimpleCommand
+data Simple = SimpleCommand
   { totalSize :: Int32        -- The size of the frame, counting everything that comes after it (in bytes)
   , commandSize :: Int32      -- The size of the protobuf-serialized command
   , message :: CL.ByteString  -- The protobuf message serialized in a raw binary format (rather than in protobuf format)
   }
 
 -- Payload command: http://pulsar.apache.org/docs/en/develop-binary-protocol/#payload-commands
-data PayloadCommand = PayloadCommand
-  { simpleCommand :: SimpleCommand  -- Total size, command size and message
-  , magicNumber :: Int32            -- A 2-byte byte array (0x0e01) identifying the current format
+data Payload = PayloadCommand
+  { magicNumber :: Int32            -- A 2-byte byte array (0x0e01) identifying the current format
   , checkSum :: Int32               -- A CRC32-C checksum of everything that comes after it
   , metadataSize :: Int32           -- The size of the message metadata
   , metadata :: CL.ByteString       -- The message metadata stored as a binary protobuf message
@@ -49,13 +53,31 @@ mkSimpleFrame cmd = SimpleFrame $
   msg     = CL.fromStrict $ PL.encodeMessage cmd
   cmdSize = fromIntegral $ CL.length msg
 
+mkPayloadFrame :: BaseCommand -> Frame
+mkPayloadFrame = undefined
+
 encodeFrame :: Frame -> C.ByteString
 encodeFrame (SimpleFrame (SimpleCommand ts cs base)) =
   let totalSize   = B.runPut $ B.putInt32be ts
       commandSize = B.runPut $ B.putInt32be cs
   in  BL.toStrict $ totalSize <> commandSize <> base
-encodeFrame (PayloadFrame _) = undefined
+encodeFrame (PayloadFrame _ _) = undefined
 
--- TODO: If it has metadata build a payload frame instead
 encodeBaseCommand :: BaseCommand -> C.ByteString
-encodeBaseCommand = encodeFrame . mkSimpleFrame
+encodeBaseCommand cmd =
+  let sf = encodeFrame . mkSimpleFrame
+      pf = encodeFrame . mkPayloadFrame
+  in  if isSimple cmd then sf cmd else pf cmd
+
+isSimple :: BaseCommand -> Bool
+isSimple cmd =
+  _bool F.maybe'producer F.vec'metadata cmd
+    && _bool F.maybe'subscribe F.vec'metadata cmd
+
+_bool
+  :: forall a b c
+   . LensLike' (Constant (Maybe a)) c (Maybe a)
+  -> LensLike' (Constant (First b)) a b
+  -> c
+  -> Bool
+_bool ls1 ls2 cmd = isNothing ((^? ls2) =<< cmd ^. ls1)
